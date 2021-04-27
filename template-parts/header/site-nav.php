ec951b58534f0ef41db8d23df9fbb4bc91cade56
Birk Thestrup Blauner <birkblauner@gmail.com>
2021-04-27 18:24:21 +0200

    nav

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -6,14 +6,28 @@
 
 ?>
 
-<?php if ( has_nav_menu( 'primary' ) ) : ?>
+<?php if ( has_nav_menu( 'primary_logged_in' ) || has_nav_menu( 'primary_logged_out' ) ) : ?>
 	<nav id="site-navigation" class="primary-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'twentytwentyone' ); ?>">
 		<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => is_user_logged_in() ? 'primary_logged_in' : 'primary_logged_out',
-			)
-		);
+		if ( has_nav_menu( 'primary_logged_in' ) && has_nav_menu( 'primary_logged_out' ) ) :
+			wp_nav_menu(
+				array(
+					'theme_location'  => is_user_logged_in() ? 'primary_logged_in' : 'primary_logged_out',
+				)
+			);
+		elseif ( has_nav_menu( 'primary_logged_out' ) ) :
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'primary_logged_out',
+				)
+			);
+		else :
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'primary_logged_in',
+				)
+			);
+		endif;
 		?>
 	</nav><!-- #site-navigation -->
 <?php endif; ?>
